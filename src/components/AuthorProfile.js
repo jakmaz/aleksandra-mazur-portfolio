4cded8ce4dac2c332c0592e8ec77d8641743bfd6
Mazur, Jacob (Stud. DACS) <jacob.mazur@student.maastrichtuniversity.nl>
2024-01-27 19:03:51 +0100

    Fix: Formatting

--- src/components/AuthorProfile.js
+++ src/components/AuthorProfile.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 function AuthorProfile() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 w-full h-svh">
+        <div className="grid w-full grid-cols-1 h-svh md:grid-cols-2">
             <div className="bg-[#E6E9E6] flex items-center justify-center">
                 <div>
-                    <h1 className="text-8xl font-bold text-gray-900 mb-4">
+                    <h1 className="mb-4 text-8xl font-bold text-gray-900">
                         Aleksandra <br/>
                         <span className="block">Mazur</span>
                     </h1>
-                    <p className="text-xl text-gray-700 mb-6">Author of the best-selling books...</p>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <p className="mb-6 text-xl text-gray-700">Author of the best-selling books...</p>
+                    <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
                         Kup powieść
                     </button>
                 </div>
             </div>
-            <div className="bg-white flex items-center justify-center">
-                <div className="w-fit mx-auto flex flex-col items-center justify-center">
+            <div className="flex items-center justify-center bg-white">
+                <div className="mx-auto flex w-fit flex-col items-center justify-center">
                     <img src="/images/author-image.jpg" alt="Author" className="w-72 rounded-md shadow-2xl"/>
-                    <div className="max-w-72 text-sm text-gray-600 mt-8">
+                    <div className="mt-8 text-sm text-gray-600 max-w-72">
                         Laureatka Ogólnopolskiego Konkursu Literackiego "Pisanie Dobre Na Chandrę" Oświęcim, 2017 rok
                     </div>
-                    <div className="text-gray-800 font-handwriting mt-2">Aleksandra</div>
+                    <div className="mt-2 text-gray-800 font-handwriting">Aleksandra</div>
                 </div>
             </div>
         </div>
