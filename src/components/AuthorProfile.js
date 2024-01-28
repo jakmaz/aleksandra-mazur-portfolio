6804df863ccb8bf0ba6c799ed463b7ffd3cb2c99
Mazur, Jacob (Stud. DACS) <jacob.mazur@student.maastrichtuniversity.nl>
2024-01-28 17:02:19 +0100

    Fix: Author profile name

--- src/components/AuthorProfile.js
+++ src/components/AuthorProfile.js
@@ -5,7 +5,13 @@ function AuthorProfile() {
         <div className="flex flex-wrap md:h-svh">
             <div className="bg-[#E6E9E6] flex items-center w-full md:w-1/2 h-full">
                 <div className="flex flex-col space-y-6 mx-auto p-10 text-center md:text-left">
-                    <h1 className=" text-3xl font-bold leading-tight text-gray-900 font-noticia lg:text-5xl xl:text-8xl">
+                    {/* This h1 will only show on medium screens and larger */}
+                    <h1 className="text-3xl md:text-5xl lg:text-8xl font-bold leading-tight text-gray-900 font-noticia">
+                        <span className="hidden md:block">Aleksandra </span>
+                        <span className="hidden md:block">Mazur</span>
+                    </h1>
+                    {/* This h1 will only show on small screens */}
+                    <h1 className="md:hidden text-3xl font-bold leading-tight text-gray-900 font-noticia">
                         Aleksandra Mazur
                     </h1>
                     <p className="text-lg text-gray-700 md:text-xl">Author of the best-selling books...</p>
@@ -14,7 +20,8 @@ function AuthorProfile() {
                     </button>
                 </div>
             </div>
-            <div className="order-first flex w-full items-center justify-center bg-white p-10 pt-2 md:p-0 md:order-last md:w-1/2">
+            <div
+                className="order-first flex w-full items-center justify-center bg-white p-10 pt-2 md:p-0 md:order-last md:w-1/2">
                 <div className="flex w-full flex-col items-center justify-center md:w-72">
                     <img src="/images/author-image.jpg" alt="Author" className="rounded-xl shadow-2xl"/>
                     <div className="mt-4 text-sm text-slate-900">
