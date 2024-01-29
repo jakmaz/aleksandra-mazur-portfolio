7b5f626cd52d6d19c7cf1de91cbace16c4804714
Mazur, Jacob (Stud. DACS) <jacob.mazur@student.maastrichtuniversity.nl>
2024-01-29 04:41:10 +0100

    Feature: Reviews page

--- src/pages/ReviewsPage.js
+++ src/pages/ReviewsPage.js
@@ -2,8 +2,64 @@ import React from "react";
 
 export default function ReviewsPage () {
     return (
-        <main>
-            <h1>Test</h1>
-        </main>
+        <div className="bg-[#E6E9E6] py-6 md:py-10 md:min-h-[calc(100vh-50px)] flex flex-col justify-center md:space-y-10">
+            <div className="flex items-center justify-center">
+                <h1 className="text-4xl font-bold font-noticia md:text-5xl">Reviews</h1>
+            </div>
+            <div
+                className="flex flex-wrap justify-center p-6 px-10 text-center space-y-10 md:space-x-10 md:space-y-0 md:flex-nowrap md:justify-center">
+                <div className="flex flex-col items-center p-6 md:w-1/4">
+                    <p>"I'm a review. Click to edit me and add text from a critic who has evaluated you and your
+                        work."</p>
+                    <div className="font-bold font-noticia">
+                        <span>─</span> <br/>
+                        <span>Seattle Daily</span>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center p-6 md:w-1/4">
+                    <p>"I'm a review. Click to edit me and add text from a critic who has evaluated you and your
+                        work."</p>
+                    <div className="font-bold font-noticia">
+                        <span>─</span> <br/>
+                        <span>Seattle Daily</span>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center p-6 md:w-1/4">
+                    <p>"I'm a review. Click to edit me and add text from a critic who has evaluated you and your
+                        work."</p>
+                    <div className="font-bold font-noticia">
+                        <span>─</span> <br/>
+                        <span>Seattle Daily</span>
+                    </div>
+                </div>
+            </div>
+            <div
+                className="flex flex-wrap justify-center p-6 px-10 text-center space-y-10 md:space-x-10 md:space-y-0 md:flex-nowrap md:justify-center">
+                <div className="flex flex-col items-center p-6 md:w-1/4">
+                    <p>"I'm a review. Click to edit me and add text from a critic who has evaluated you and your
+                        work."</p>
+                    <div className="font-bold font-noticia">
+                        <span>─</span> <br/>
+                        <span>Seattle Daily</span>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center p-6 md:w-1/4">
+                    <p>"I'm a review. Click to edit me and add text from a critic who has evaluated you and your
+                        work."</p>
+                    <div className="font-bold font-noticia">
+                        <span>─</span> <br/>
+                        <span>Seattle Daily</span>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center p-6 md:w-1/4">
+                    <p>"I'm a review. Click to edit me and add text from a critic who has evaluated you and your
+                        work."</p>
+                    <div className="font-bold font-noticia">
+                        <span>─</span> <br/>
+                        <span>Seattle Daily</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
