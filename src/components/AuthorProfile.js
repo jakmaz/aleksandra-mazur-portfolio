34024f52042aac46155cdad591ab48af8e4e1256
Mazur, Jacob (Stud. DACS) <jacob.mazur@student.maastrichtuniversity.nl>
2024-01-29 04:22:52 +0100

    Fix: Minor styling fixes

--- src/components/AuthorProfile.js
+++ src/components/AuthorProfile.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 function AuthorProfile() {
     return (
-        <div className="flex flex-wrap md:h-svh">
-            <div className="bg-[#E6E9E6] flex items-center justify-center w-full md:w-1/2 h-full">
-                <div className="flex flex-col space-y-6 p-10 text-center md:text-left">
+        <div className="flex flex-wrap md:min-h-[calc(100vh-50px)]">
+            <div className="bg-[#E6E9E6] flex items-center justify-center w-full md:w-1/2">
+                <div className="flex flex-col p-10 text-center space-y-6 md:text-left">
                     {/* This h1 will only show on medium screens and larger */}
-                    <h1 className="text-3xl md:text-5xl lg:text-8xl font-bold leading-tight text-gray-900 font-noticia">
+                    <h1 className="text-3xl font-bold leading-tight text-gray-900 font-noticia md:text-5xl lg:text-8xl">
                         <span className="hidden md:block">Aleksandra </span>
                         <span className="hidden md:block">Mazur</span>
                     </h1>
                     {/* This h1 will only show on small screens */}
-                    <h1 className="md:hidden text-3xl font-bold leading-tight text-gray-900 font-noticia">
+                    <h1 className="text-3xl font-bold leading-tight text-gray-900 font-noticia md:hidden">
                         Aleksandra Mazur
                     </h1>
                     <p className="text-lg text-gray-700 md:text-xl">Author of the best-selling books...</p>
-                    <button className="md:w-32 rounded bg-white px-4 py-2 hover:bg-slate-100">
+                    <button className="rounded bg-white px-4 py-2 hover:bg-slate-100 md:w-32">
                         Kup powieść
                     </button>
                 </div>
             </div>
             <div
-                className="order-first flex w-full items-center justify-center bg-white p-10 pt-2 md:p-0 md:order-last md:w-1/2">
+                className="order-first flex w-full items-center justify-center bg-white p-10 pt-2 md:order-last md:w-1/2 md:p-0">
                 <div className="flex w-full flex-col items-center justify-center md:w-72">
                     <img src="/images/author-image.jpg" alt="Author" className="rounded-xl shadow-2xl"/>
                     <div className="mt-4 text-sm text-slate-900">
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 export default function AboutPage () {
     return (
-        <div className="flex flex-col md:flex-row items-center px-8 md:space-x-20 justify-center py-4 md:py-12 bg-white">
-            <div className="md:w-1/4 flex justify-center">
+        <div className="flex flex-col md:flex-row items-center px-8 md:space-x-20 space-y-10 justify-center pt-4 pb-16 md:p-10 md:min-h-[calc(100vh-50px)] bg-white">
+            <div className="flex justify-center md:w-1/4">
                 <img
                     src="/images/about-image.jpg"
                     alt="Portrait of Aleksandra Mazur"
-                    className="mb-8 md:mb-0 rounded-xl"
+                    className="rounded-xl"
                 />
             </div>
-            <div className="md:w-1/2 2xl:w-1/4 flex flex-col font-noticia">
-                <h2 className="text-5xl font-noticia font-bold mb-6">About</h2>
-                <p className="text-lg mb-4">
+            <div className="flex flex-col font-noticia md:w-1/2 2xl:w-1/4">
+                <h2 className="mb-6 text-5xl font-bold font-noticia">About</h2>
+                <p className="mb-4 text-lg">
                     I'm a paragraph. Click here to add your own text and edit me. Let your users get to know you.
                 </p>
-                <p className="text-lg mb-4">
+                <p className="mb-4 text-lg">
                     I'm a paragraph. Click here to add your own text and edit me. It's easy. Just click "Edit Text" or double
                     click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like
                     on your page. I'm a great place for you to tell a story and let your users know a little more about you.
