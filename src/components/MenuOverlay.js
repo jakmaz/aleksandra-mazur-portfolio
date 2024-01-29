7e52df7ed062aa1ff79c82a025e074750c078942
Mazur, Jacob (Stud. DACS) <jacob.mazur@student.maastrichtuniversity.nl>
2024-01-29 04:03:06 +0100

    Feature: About Page

--- src/components/MenuOverlay.js
+++ src/components/MenuOverlay.js
@@ -2,8 +2,8 @@ export default function MenuOverlay() {
     return (
         <div className="flex flex-col space-y-2 mb-3 items-center">
             <a href="/" className="text-gray-800 transition-colors hover:text-gray-600">Strona Główna</a>
-            <a href="/src/pages/AboutPage" className="text-gray-800 transition-colors hover:text-gray-600">O mnie</a>
-            <a href="/src/pages/BooksPage" className="text-gray-800 transition-colors hover:text-gray-600">Książki</a>
+            <a href="/about" className="text-gray-800 transition-colors hover:text-gray-600">O mnie</a>
+            <a href="/books" className="text-gray-800 transition-colors hover:text-gray-600">Książki</a>
             <a href="/reviews" className="text-gray-800 transition-colors hover:text-gray-600">Recenzje</a>
             <a href="/contact" className="text-gray-800 transition-colors hover:text-gray-600">Kontakt</a>
         </div>
